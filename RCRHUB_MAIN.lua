aef066cfb54b9132a32213e4cd79b3e3d329c303
glrishikesh-maker <glrishikesh@gmail.com>
2026-01-31 23:02:53 +0000

    Update RCRHUB_MAIN.lua

--- RCRHUB_MAIN.lua
+++ RCRHUB_MAIN.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "RCR HUB",
    LoadingTitle = "RCR System",
-   LoadingSubtitle = "by me",
+   LoadingSubtitle = "by gold3en_rishi",
    ConfigurationSaving = { Enabled = false },
    Discord = {
       Enabled = true,
